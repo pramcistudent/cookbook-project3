1eb758b23567a9b00e1da7e0caff2f803c398226
Ubuntu <ubuntu@ip-172-31-92-146.ec2.internal>
2019-10-22 19:09:31 +0000

    Route name changes to app.py

--- app.py
+++ app.py
@@ -10,17 +10,19 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI", 'mongodb://localhost')
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/all_recipes')
 def all_recipes():
-    return render_template("allrecipes.html",
-    dishes=mongo.db.dishes.find())
+    return render_template("index.html",
+    recipes=mongo.db.recipes.find())
     
-@app.route('/recipe_form')
-def recipe_form():
-    return render_template("addrecipe.html",
-    allergens=mongo.db.allergens.find(),
-    cuisines=mongo.db.cuisines.find(),
-    dishes=mongo.db.dishes.find())
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html")
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes=mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for("all_recipes"))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
