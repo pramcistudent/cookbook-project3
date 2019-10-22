cf111a87f5fc072bdbc6332c7e7cfd6db9be0c61
Ubuntu <ubuntu@ip-172-31-92-146.ec2.internal>
2019-10-22 18:00:20 +0000

    Added allergens form to addrecipe.html

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ def all_recipes():
     
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template("addrecipe.html")
+    return render_template("addrecipe.html",
+    allergens=mongo.db.allergens.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
