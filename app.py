f0ed46f97c1e8a30dd5c79eade13c1b3b8d59438
Ubuntu <ubuntu@ip-172-31-92-146.ec2.internal>
2019-10-22 18:09:25 +0000

    Added cuisine & dish type from to addrecipe.html

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, url_for, request
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'cookbook'
@@ -11,13 +12,15 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/all_recipes')
 def all_recipes():
-    return render_template("index.html",
+    return render_template("allrecipes.html",
     dishes=mongo.db.dishes.find())
     
-@app.route('/add_recipe')
-def add_recipe():
+@app.route('/recipe_form')
+def recipe_form():
     return render_template("addrecipe.html",
-    allergens=mongo.db.allergens.find())
+    allergens=mongo.db.allergens.find(),
+    cuisines=mongo.db.cuisines.find(),
+    dishes=mongo.db.dishes.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
