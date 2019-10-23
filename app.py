a2a463a266ae1fa6a625fbe6d20e79caddfe2db7
Ubuntu <ubuntu@ip-172-31-92-146.ec2.internal>
2019-10-23 18:05:34 +0000

    Amend app.py and remove insert cuisine/dish function

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def update_recipe(recipe_id):
         'recipe_title':request.form.get('recipe_title'),
         'recipe_short_description':request.form.get('recipe_short_description'),
         'recipe_image_url': request.form.get('recipe_image_url'),
-        'cuisine_name': request.form.getlist('cuisine_name'),
-        'dish_type':request.form.getlist('dish_type'),
+        'cuisine_name': request.form.get('cuisine_name'),
+        'dish_type':request.form.get('dish_type'),
         'allergen_name':request.form.getlist('allergen_name'),
         'recipe_prep_time':request.form.get('recipe_prep_time'),
         'recipe_cook_time':request.form.get('recipe_cook_time'),
@@ -71,29 +71,8 @@ def insert_recipe():
     recipe = doc
     recipes=mongo.db.recipes 
     recipes.insert_one(recipe)
-    insert_cuisine()
-    insert_dish()
     return redirect(url_for('all_recipes'))
 
-def insert_cuisine():
-    doc2 ={}
-    data = request.form.items()
-    for k, v in data:
-        if k == 'cuisine_name':
-            doc2[k] = v
-            cuisine = doc2
-            cuisines=mongo.db.cuisines
-            cuisines.insert_one(cuisine)
-
-def insert_dish():
-    doc3 ={}
-    data = request.form.items()
-    for k, v in data:
-        if k == 'dish_type':
-            doc3[k] = v
-            dish = doc3
-            dishes=mongo.db.dishes
-            dishes.insert_one(dish)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
