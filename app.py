f8a882e365c692c7ac01447aadb3cd545fb4c059
Ubuntu <ubuntu@ip-172-31-92-146.ec2.internal>
2019-11-02 20:10:36 +0000

    Refactored app.py

--- app.py
+++ app.py
@@ -38,11 +38,12 @@ def signup():
 # Check data submitted via Registration form
 @app.route('/register', methods=['POST'])
 def register():
-    users = mongo.db.users
     fullname = request.form.get('fullname')
     username = request.form.get('username')
     password = request.form.get('password')
-    registered = users.find_one({'username': {'$regex': username, '$options': 'i'}})
+    registered = users.find_one({
+        'username': {'$regex': username, '$options': 'i'}
+    })
     if registered is None:
         users.insert_one({
             'username': username,
@@ -50,17 +51,19 @@ def register():
             'password': password
         })
         success = True
-        return render_template('index.html', success = success)
+        return render_template('index.html', success=success)
     success = False
-    return render_template('index.html', success = success)
+    return render_template('index.html', success=success)
 
 # Check data submitted via Login form
 @app.route('/logout', methods=['POST'])
 def login():
-    users = mongo.db.users
     username = request.form.get('username')
     password = request.form.get('password')
-    registered = users.find_one({'username': {'$regex': username, '$options': 'i'}, 'password': password})
+    registered = users.find_one({
+        'username': {'$regex': username, '$options': 'i'}, 
+        'password': password
+    })
     if registered is not None:
         session['username'] = username
         login = True
@@ -84,22 +87,25 @@ def logout():
 def my_recipes(username, num):
     if username is not None:
         my_recipes = recipes.find({
-            'recipe_author_name': {'$regex': username, '$options': 'i'}}) 
-        total_my_recipes=my_recipes.count()
+            'recipe_author_name': {'$regex': username, '$options': 'i'}
+        })
+        total_my_recipes = my_recipes.count()
+        
     total_pages = range(1, math.ceil(total_my_recipes/8) + 1)
-    skip_num = 8 * (int(num)-1)
+    skip_num = 8 * (int(num) - 1)
     recipes_per_page = my_recipes.skip(skip_num).limit(8)
+    
     if total_my_recipes <= 8:
         page_count = total_my_recipes
-    elif (int(num) * 8) < total_my_recipes:
-        page_count = int(num) * 8
+    elif (skip_num + 8) <= total_my_recipes:
+        page_count = skip_num + 8
     else:
-        page_count = total_my_recipes - skip_num
+        page_count = total_my_recipes 
     return render_template("myrecipes.html", recipes=recipes.find(), dishes=dishes.find(), 
                     cuisines=cuisines.find(), allergens=allergens.find(), my_recipes=my_recipes, 
                     users=users.find(), total_pages=total_pages, num=num, skip_num=skip_num, 
                     page_count=page_count, total_my_recipes=total_my_recipes,
-                    recipes_per_page=recipes_per_page)
+                    recipes_per_page=recipes_per_page)  
 
 # Home page displaying all uploaded recipes
 @app.route('/all_recipes/page:<num>')
@@ -108,6 +114,7 @@ def all_recipes(num):
     total_pages = range(1, math.ceil(total_recipes/8) + 1)
     skip_num = 8 * (int(num)-1)
     recipes_per_page = recipes.find().skip(skip_num).limit(8)
+    
     if total_recipes <= 8:
         page_count = total_recipes
     elif (skip_num + 8) <= total_recipes:
@@ -119,27 +126,28 @@ def all_recipes(num):
             allergens=allergens.find(), total_pages=total_pages, skip_num=skip_num, 
             num=num, page_count=page_count, recipes_per_page=recipes_per_page, 
             total_recipes=total_recipes)
-
+    
 # Displays detail view of a recipe
 @app.route('/the_recipe/<recipe_id>/<recipe_title>')
 def the_recipe(recipe_id, recipe_title):
-    return render_template("recipe.html", recipe = recipes.find_one(
-            {'_id': ObjectId(recipe_id),'recipe_title': recipe_title}),
-            cuisines=cuisines.find(), dishes=dishes.find(), allergens=allergens.find(), 
-            users=users.find())
+    return render_template("recipe.html", cuisines=cuisines.find(),  
+            allergens=allergens.find(), users=users.find(), dishes=dishes.find(),
+            recipe=recipes.find_one({'_id': ObjectId(recipe_id),
+            'recipe_title': recipe_title}))
 
 # Display form to add a recipe
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template("addrecipe.html", cuisines=cuisines.find(), dishes=dishes.find(), 
-            recipes=recipes.find(), allergens=allergens.find(), users=users.find())
+    return render_template("addrecipe.html", cuisines=cuisines.find(), 
+            dishes=dishes.find(), recipes=recipes.find(), users=users.find(), 
+            allergens=allergens.find())
 
 # Display form to edit the recipe
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
-    return render_template('editrecipe.html', recipe = recipes.find_one({"_id": ObjectId(recipe_id)}),
-            cuisines = cuisines.find(), dishes = dishes.find(), allergens = allergens.find(), 
-            users=users.find())
+    return render_template('editrecipe.html', cuisines=cuisines.find(), 
+            dishes=dishes.find(), allergens=allergens.find(), users=users.find(),
+            recipe=recipes.find_one({"_id": ObjectId(recipe_id)}))
 
 # Send form data to update recipe in DB
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
@@ -167,11 +175,12 @@ def update_recipe(recipe_id):
 # Insert new recipe to DB collection
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-    doc ={'recipe_author_name': session['username']}
+    doc = {'recipe_author_name': session['username']}
     data = request.form.items()
     all_ingred = request.form.getlist('ingred')
     all_steps = request.form.getlist('steps')
     all_allergens = request.form.getlist("allergen_name")
+   
     for k, v in data: 
         if k == "ingred":
             doc["recipe_ingredients"]=all_ingred
@@ -179,12 +188,12 @@ def insert_recipe():
             doc["recipe_steps"]=all_steps
         elif k == "allergen_name":
             doc["allergen_name"] = all_allergens
-        else:    
-            doc[k]= v
+        else:
+            doc[k]= v  
 
     new_recipe = doc
     recipes.insert_one(new_recipe)
-    return redirect(url_for('all_recipes', num=1))
+    return redirect(url_for('my_recipes', username=session['username'], num=1))
 
 # Removes a recipe from DB
 @app.route('/delete_recipe/<recipe_id>')
@@ -198,18 +207,18 @@ def search_dish(dish_type, num):
     dish_result = recipes.find({'dish_type': dish_type})
     dish_count = dish_result.count()
     total_pages = range(1, math.ceil(dish_count/8) + 1)
-    skip_num = 8 * (int(num)-1)
+    skip_num = 8 * (int(num) - 1)
     recipes_per_page = dish_result.skip(skip_num).limit(8)
     if dish_count <= 8:
         page_count = dish_count
-    elif (int(num) * 8) < dish_count:
-        page_count = int(num) * 8
+    elif (skip_num + 8) <= dish_count:
+        page_count = skip_num + 8
     else:
-        page_count = dish_count - skip_num
+        page_count = dish_count
     return render_template('searchdish.html', dish_type=dish_type, num=num, 
             total_pages=total_pages, page_count=page_count, skip_num=skip_num, 
-            recipes_per_page=recipes_per_page, count = dish_count, dishes=dishes.find(), 
-            cuisines=cuisines.find(), users=users.find(), allergens=allergens.find()) 
+            recipes_per_page=recipes_per_page, count=dish_count, dishes=dishes.find(), 
+            cuisines=cuisines.find(), users=users.find(), allergens=allergens.find())  
 
 # Search by Cuisine 
 @app.route('/search_cuisine/<cuisine_name>/page:<num>')
@@ -217,33 +226,36 @@ def search_cuisine(cuisine_name, num):
     cuisine_result = recipes.find({'cuisine_name': cuisine_name})
     cuisine_count = cuisine_result.count()
     total_pages = range(1, math.ceil(cuisine_count/8) + 1)
-    skip_num = 8 * (int(num)-1)
+    skip_num = 8 * (int(num) - 1)
     recipes_per_page = cuisine_result.skip(skip_num).limit(8)
-    if cuisine_count < 8:
+    
+    if cuisine_count <= 8:
         page_count = cuisine_count
-    elif (int(num) * 8) < cuisine_count:
-        page_count = int(num) * 8
+    elif (skip_num + 8) <= cuisine_count:
+        page_count = skip_num + 8
     else:
-        page_count = cuisine_count - skip_num
-    return render_template('searchcuisine.html', recipes_per_page = recipes_per_page, 
-            num=num, cuisine_name = cuisine_name,  skip_num=skip_num, total_pages=total_pages, 
-            page_count=page_count, count = cuisine_count, cuisines=cuisines.find(), 
+        page_count = cuisine_count 
+    return render_template('searchcuisine.html', recipes_per_page=recipes_per_page, 
+            num=num, cuisine_name=cuisine_name,  skip_num=skip_num, total_pages=total_pages, 
+            page_count=page_count, count=cuisine_count, cuisines=cuisines.find(), 
             dishes=dishes.find(), users=users.find(), allergens=allergens.find())
 
 # Search by Allergens
 @app.route('/search_allergen/<allergen_name>/page:<num>')
 def search_allergen(allergen_name, num):
-    allergen_result = recipes.find({'allergen_name':{'$not': {'$eq': allergen_name}}})
+    allergen_result = recipes.find({'allergen_name':
+        {'$not': {'$eq': allergen_name}}})
     allergen_count = allergen_result.count()
     total_pages = range(1, math.ceil(allergen_count/8) + 1)
     skip_num = 8 * (int(num)-1)
     recipes_per_page = allergen_result.skip(skip_num).limit(8)
+    
     if allergen_count <= 8:
         page_count = allergen_count
-    elif (int(num) * 8) < allergen_count:
-        page_count = int(num) * 8
+    elif (skip_num + 8) <= allergen_count:
+        page_count = skip_num + 8
     else:
-        page_count = allergen_count - skip_num
+        page_count = allergen_count
     return render_template('searchallergen.html', num=num, allergen_name=allergen_name, 
             total_pages = total_pages, skip_num=skip_num, page_count=page_count, 
             recipes_per_page=recipes_per_page, count=allergen_count, dishes=dishes.find(), 
